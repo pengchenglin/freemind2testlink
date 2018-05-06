7a7fbf20f776f0ec509f93c68ed9133d42d6101c
LiN <m170587788@163.com>
2018-05-06 20:42:54 +0800

    add readme

---
+++ Freemind.py
@@ -0,0 +1,233 @@
+# -*- coding: utf-8 -*-
+import logging.config
+import os
+from xml.etree import ElementTree as ET
+from lxml import etree as lxmlET
+
+
+def CDATA(text=None):
+    element = ET.Element('![CDATA[')
+    element.text = text
+    return element
+
+
+def get_freemindfile():
+    L = []
+    for root, dirs, files in os.walk('./'):
+        for file in files:
+            if os.path.splitext(file)[1] == '.mm':
+                # L.append(os.path.join(root, file))
+                L.append(file)
+
+        if len(L) == 0:
+                logging.error('There is no FreeMind file,Please check out!')
+        return L
+
+
+class FreeMind(object):
+    def __init__(self, logger, ):
+        self.logger = logger
+        self.log_prefix = 'FreeMind:'
+
+    def Generate_TCs_from_TDS(self, file, node_list='', reuse_tc='0', link_rule='BY_TDS_ID_THEN_SMART_NAME',
+                              suite_structure='TDS'):
+        """
+        The generated xml file need to be imported into testlink manually.
+        """
+        self.logger.info(self.log_prefix + "Starting to export %s to xml file." % file)
+        tc_tds_dict = {}
+        tc_pfs_dict = {}
+
+        fm_tree = lxmlET.parse(file)
+        tds_root = fm_tree.getroot()
+        node_list = node_list.split('|')
+        node_list = [item.strip() for item in node_list]
+
+        tc_root = lxmlET.Element('testsuite', {'name': ''})
+        lxmlET.SubElement(tc_root, 'node_order').text = lxmlET.CDATA('')
+        lxmlET.SubElement(tc_root, 'details').text = lxmlET.CDATA('')
+        self._gen_tc_xml_from_tds(tc_root, tds_root, tc_tds_dict, tc_pfs_dict, node_list, reuse_tc, link_rule,
+                                  suite_structure)
+        xml_file_name = file[:-2] + 'xml'
+
+        f = open(xml_file_name, 'wb')
+        f.write(lxmlET.tostring(tc_root, xml_declaration=True, encoding='utf-8', pretty_print=True))
+        f.close()
+        self.logger.info(self.log_prefix + \
+                         "Successfully generated the test cases xml file (%s).\n\n\n\n" % xml_file_name)
+
+    def _gen_tc_xml_from_tds(self, ts_node, root_node, tc_tds_dict, tc_pfs_dict, node_list,
+                             reuse_tc, link_rule, suite_structure):
+        existing_tc_list = []
+        if node_list == ['']:
+            self.logger.info(self.log_prefix + \
+                             "Generating test cases xml file for all TDS nodes.")
+            self._gen_tc_xml_from_tds_node(ts_node, root_node, tc_tds_dict, tc_pfs_dict, existing_tc_list,
+                                           reuse_tc, link_rule, suite_structure)
+            return
+        for tds_item in root_node.iter('node'):
+            if tds_item.attrib['ID'] in node_list:
+                self.logger.info(self.log_prefix + \
+                                 "Generating test cases xml file for TDS node (%s)." % \
+                                 (tds_item.attrib['ID']))
+                child_ts_node = lxmlET.SubElement(ts_node, 'testsuite', {'name': tds_item.attrib['TEXT'].strip()})
+                lxmlET.SubElement(child_ts_node, 'node_order').text = lxmlET.CDATA('')
+                lxmlET.SubElement(child_ts_node, 'details').text = lxmlET.CDATA('')
+                self._gen_tc_xml_from_tds_node(child_ts_node, tds_item, tc_tds_dict, tc_pfs_dict, existing_tc_list,
+                                               reuse_tc, link_rule, suite_structure)
+
+    def _gen_tc_xml_from_tds_node(self, ts_node, root_node, tc_tds_dict, tc_pfs_dict, existing_tc_list,
+                                  reuse_tc, link_rule, suite_structure):
+        ts_node_order = -1
+        tc_node_order = -1
+        for tds_item in root_node.findall('node'):
+            if tds_item.attrib.has_key('LINK'):
+                continue
+            is_testsuite = False
+            has_steps = False
+            for item_icon in tds_item.findall('icon'):
+                if item_icon.attrib['BUILTIN'] == 'flag':
+                    ts_node_order += 1
+                    child_ts_node = lxmlET.SubElement(ts_node, 'testsuite', {'name': tds_item.attrib['TEXT'].strip()})
+                    self.logger.info(
+                        self.log_prefix + "Creating Test Suite   [%s]" % tds_item.attrib['TEXT'].strip())
+                    lxmlET.SubElement(child_ts_node, 'node_order').text = lxmlET.CDATA(str(ts_node_order))
+                    lxmlET.SubElement(child_ts_node, 'details').text = lxmlET.CDATA('')
+                    # ts_node = child_ts_node
+                    is_testsuite = True
+                    break
+                '''如果有星星的，后面的是步骤'''
+                if item_icon in tds_item.findall('icon'):
+                    if item_icon.attrib['BUILTIN'] == 'bookmark':
+                        tc_node_order += 1
+                        step_ts_node = lxmlET.SubElement(ts_node, 'testcase',
+                                                         {'name': tds_item.attrib['TEXT'].strip()})
+                        has_steps = True
+                        break
+
+            if is_testsuite:
+                self._gen_tc_xml_from_tds_node(child_ts_node, tds_item, tc_tds_dict, tc_pfs_dict, existing_tc_list,
+                                               reuse_tc, link_rule, suite_structure)
+                continue
+            '''存在步骤的，添加步骤内容'''
+            if has_steps:
+                self.logger.info(
+                    self.log_prefix + "Generating Test case       -<has steps> %s" % tds_item.attrib['TEXT'])
+                lxmlET.SubElement(step_ts_node, 'node_order').text = lxmlET.CDATA(str(tc_node_order))
+                lxmlET.SubElement(step_ts_node, 'externalid').text = lxmlET.CDATA('')
+                lxmlET.SubElement(step_ts_node, 'version').text = lxmlET.CDATA('1')
+                lxmlET.SubElement(step_ts_node, 'summary').text = lxmlET.CDATA(tds_item.attrib['TEXT'].strip())
+                lxmlET.SubElement(step_ts_node, 'preconditions').text = lxmlET.CDATA('')
+                lxmlET.SubElement(step_ts_node, 'execution_type').text = lxmlET.CDATA('1')
+
+                node_reg_lvl = 2
+                for node_icon in tds_item.findall('icon'):
+                    if node_icon.attrib['BUILTIN'].strip().count('full-') == 1:
+                        node_reg_lvl = 4 - int(node_icon.attrib['BUILTIN'].strip()[-1])
+                if node_reg_lvl < 1:
+                    node_reg_lvl = 2
+
+                lxmlET.SubElement(step_ts_node, 'importance').text = lxmlET.CDATA(str(node_reg_lvl))
+                # print(type(step_ts_node))
+                '''xml写入steps'''
+                steps = lxmlET.SubElement(step_ts_node, 'steps')
+                '''循环写入step'''
+                sn = 1
+                for item in tds_item.findall('node'):
+
+                    step = lxmlET.SubElement(steps, 'step')
+                    lxmlET.SubElement(step, 'step_number').text = lxmlET.CDATA(str(sn))
+
+                    '''步骤和期望结果用 & 符号区分'''
+                    stritem = str(item.attrib['TEXT']).split('&')
+                    # print(stritem)
+                    lxmlET.SubElement(step, 'actions').text = lxmlET.CDATA(stritem[0])
+                    self.logger.info(self.log_prefix + "add Step actions               %s" % stritem[0])
+                    if len(stritem) > 1:
+                        lxmlET.SubElement(step, 'expectedresults').text = lxmlET.CDATA(stritem[1])
+                        self.logger.info(self.log_prefix + "add Expected Results           ->%s" % stritem[1])
+                    lxmlET.SubElement(step, 'execution_type').text = lxmlET.CDATA('1')
+                    sn += 1
+                continue
+
+            if not self._last_tds_node(tds_item):
+                self._gen_tc_xml_from_tds_node(ts_node, tds_item, tc_tds_dict, tc_pfs_dict, existing_tc_list,
+                                               reuse_tc, link_rule, suite_structure)
+                continue
+            # This must be the last TDS node
+            tc_list = []
+            # res = self._get_linked_tc(tds_item, tc_list)
+            if not tc_list:
+                # There is no linked test case nodes (which mainly used for reusing test cases between projects)
+                if tds_item.attrib['ID'].strip() in existing_tc_list:
+                    continue
+                existing_tc_list.append(tds_item.attrib['ID'].strip())
+                tc_node_order += 1
+                self._add_dummy_testcase(ts_node, tds_item, tc_tds_dict, tc_pfs_dict, tc_node_order)
+
+            # If this node already have test cases associated, update its traceability if necessary.
+            # Get the test case from original xml file and copy it into the new xml file
+            for tc_id in tc_list:
+                if tc_id in existing_tc_list:
+                    continue
+                existing_tc_list.append(tc_id)
+                tc_node_order += 1
+                # tc_node = self._get_tc_node_from_xml_by_id(self.based_tc_url, tc_id)
+                if tc_node is None:
+                    return
+                # res = self._update_tc_node(tc_node, tc_node_order, tds_item, tc_tds_dict, tc_pfs_dict, tc_id)
+                ts_node.append(tc_node)
+
+    def _add_dummy_testcase(self, ts_node, tds_item, tc_tds_dict, tc_pfs_dict, tc_node_order):
+        if not tds_item.attrib.has_key('TEXT'):
+            self.logger.error(self.log_prefix + \
+                              "Please check node (%s) since it may use a long name. Please convert it to plain text via FreeMind Menu Format=>Use Plaine Text." % \
+                              (tds_item.attrib['ID'].strip()))
+            exit(-1)
+        self.logger.info(self.log_prefix + "Generating test case       -%s" % tds_item.attrib['TEXT'])
+        node_reg_lvl = 2
+        for node_icon in tds_item.findall('icon'):
+            if node_icon.attrib['BUILTIN'].strip().count('full-') == 1:
+                node_reg_lvl = 4 - int(node_icon.attrib['BUILTIN'].strip()[-1])
+            if node_reg_lvl < 1:
+                node_reg_lvl = 2
+        testcase = lxmlET.SubElement(ts_node, 'testcase', {'name': tds_item.attrib['TEXT'].strip()})
+        lxmlET.SubElement(testcase, 'node_order').text = lxmlET.CDATA(str(tc_node_order))
+        lxmlET.SubElement(testcase, 'externalid').text = lxmlET.CDATA('')
+        lxmlET.SubElement(testcase, 'version').text = lxmlET.CDATA('1')
+        lxmlET.SubElement(testcase, 'summary').text = lxmlET.CDATA(tds_item.attrib['TEXT'].strip())
+        lxmlET.SubElement(testcase, 'preconditions').text = lxmlET.CDATA('')
+        lxmlET.SubElement(testcase, 'execution_type').text = lxmlET.CDATA('1')
+        lxmlET.SubElement(testcase, 'importance').text = lxmlET.CDATA(str(node_reg_lvl))
+
+    def _last_tds_node(self, node):
+        if node.attrib.has_key('LINK'):
+            # This maybe a PFS/TC item, we need ignore it
+            return False
+        for child in node.findall('node'):
+            if not (child.attrib.has_key('LINK')):
+                return False
+        return True
+
+
+def start_main():
+    logging.basicConfig(handlers=[logging.FileHandler('logger.log', 'w', 'utf-8')],
+                        format='%(asctime)s:%(levelname)s  %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S',
+                        level=logging.DEBUG,
+                        )
+    logger = logging.getLogger(__name__)
+    console = logging.StreamHandler()  # 定义console handler
+    console.setLevel(logging.INFO)  # 定义该handler级别
+    formatter = logging.Formatter('%(asctime)s  %(filename)s : %(levelname)s  %(message)s')  # 定义该handler格式
+    console.setFormatter(formatter)
+    # Create an instance
+    logging.getLogger().addHandler(console)
+
+    file_list = get_freemindfile()
+    for file in file_list:
+        FreeMind(logger).Generate_TCs_from_TDS(file)
+
+
+if __name__ == '__main__':
+    start_main()
---
+++ demo.mm
@@ -0,0 +1,50 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1522585011903" ID="34kg4ce6uaquqpl6tfr7jgvkkq" MODIFIED="1523984465570" TEXT="&#x4e2d;&#x5fc3;&#x4e3b;&#x9898; &#x9876;&#x7ea7;node id">
+<node CREATED="1522585011903" ID="73n05rhjjp7cfrpui57g7ggo5r" MODIFIED="1522590197088" POSITION="right" TEXT="&#x6d4b;&#x8bd5;&#x96c6;&#x5408;1">
+<icon BUILTIN="folder"/>
+<node CREATED="1522585011904" ID="4sng9plapsc3hu8ev50irm8qgj" MODIFIED="1523977956792" TEXT="&#x6d4b;&#x8bd5;&#x7528;&#x4f8b;001">
+<icon BUILTIN="bookmark"/>
+<icon BUILTIN="full-1"/>
+<node CREATED="1522585011904" ID="2rv5f2kmgscci1eern2slu8dmp" MODIFIED="1523984820097" TEXT="&#x6b65;&#x9aa4;1"/>
+<node CREATED="1522585011904" ID="7u960e67f18t8j4dv6sl4n6nk5" MODIFIED="1523984824497" TEXT="&#x6b65;&#x9aa4;2"/>
+<node CREATED="1522585011904" ID="3iolmkjpe5h272si9ac0utikt9" MODIFIED="1523984831024" TEXT="&#x6b65;&#x9aa4;3"/>
+</node>
+<node CREATED="1522585011904" ID="60409vp12vs1fcl0j0dtcbgfa2" MODIFIED="1522590298452" TEXT="&#x6d4b;&#x8bd5;&#x7528;&#x4f8b;002">
+<icon BUILTIN="full-2"/>
+</node>
+<node CREATED="1522585011904" ID="04o33bru4ea976ovvjabofrdie" MODIFIED="1523977966694" TEXT="&#x6d4b;&#x8bd5;&#x7528;&#x4f8b;003">
+<icon BUILTIN="full-3"/>
+<icon BUILTIN="bookmark"/>
+<node CREATED="1522585011905" ID="2rg17dprhgm1vjeabmu0ids5t7" MODIFIED="1523977866803" TEXT="&#x6253;&#x5f00;&#x51b0;&#x7bb1;&amp;&#x6253;&#x5f00;&#x6210;&#x529f;"/>
+<node CREATED="1522585011905" ID="7ql168f354vp6o39j14bantqqs" MODIFIED="1523977885140" TEXT="&#x585e;&#x8fdb;&#x5927;&#x8c61;&amp;&#x585e;&#x5f97;&#x8fdb;&#x53bb;"/>
+<node CREATED="1522585011905" ID="7qhcqi24eekkrr7btnimt467g1" MODIFIED="1523977898800" TEXT="&#x5173;&#x4e0a;&#x51b0;&#x7bb1;&amp;&#x53ef;&#x4ee5;&#x5173;&#x4e0a;"/>
+</node>
+</node>
+<node CREATED="1522585011905" ID="5gtqm6ad30p2iblfjqmd35ag8v" MODIFIED="1522590247384" POSITION="right" TEXT="Suite2">
+<icon BUILTIN="folder"/>
+<node CREATED="1522585011905" ID="2erqtolic3ljm9ntpbpi1cbo6m" MODIFIED="1522589241900" TEXT="little suite">
+<icon BUILTIN="folder"/>
+<node CREATED="1522588270943" ID="ID_968369089" MODIFIED="1523977995189" TEXT="little suite     case1 withsstep">
+<icon BUILTIN="bookmark"/>
+<icon BUILTIN="full-1"/>
+<node CREATED="1522588364527" ID="ID_1875968868" MODIFIED="1523978044935" TEXT="step1&amp;result1"/>
+<node CREATED="1522588368567" ID="ID_1704749926" MODIFIED="1523978052885" TEXT="step2&amp;result2"/>
+<node CREATED="1522588378753" ID="ID_152789659" MODIFIED="1523978059827" TEXT="step3&amp;result3"/>
+</node>
+<node CREATED="1522588287690" ID="ID_1032732812" MODIFIED="1523977986339" TEXT="little suite     case2">
+<icon BUILTIN="full-2"/>
+</node>
+<node CREATED="1522588292294" ID="ID_38427287" MODIFIED="1523977988948" TEXT="little suite     case3">
+<icon BUILTIN="full-3"/>
+</node>
+</node>
+<node CREATED="1522585011905" ID="4dbd5nt6gghftbnqmkit19ditt" MODIFIED="1522590279484" TEXT="suite2 case001">
+<icon BUILTIN="full-5"/>
+</node>
+<node CREATED="1522585011905" ID="5uhrbvunjqu3igglq1asa6kvjq" MODIFIED="1522590282749" TEXT="suite2 case002">
+<icon BUILTIN="full-6"/>
+</node>
+</node>
+</node>
+</map>
